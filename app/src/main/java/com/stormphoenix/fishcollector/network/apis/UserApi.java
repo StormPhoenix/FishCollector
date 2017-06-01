6a959d0714117d0ced2c6e0adcfb369cb0b57a85
StormPhoenix <347861304@qq.com>
2017-06-01 20:22:07 +0800

    commit - 4

--- app/src/main/java/com/stormphoenix/fishcollector/network/apis/UserApi.java
+++ app/src/main/java/com/stormphoenix/fishcollector/network/apis/UserApi.java
@@ -2,7 +2,6 @@ package com.stormphoenix.fishcollector.network.apis;
 
 import com.stormphoenix.fishcollector.mvp.model.beans.MonitoringSite;
 import com.stormphoenix.fishcollector.network.HttpResult;
-import com.stormphoenix.fishcollector.network.model.Group;
 import com.stormphoenix.fishcollector.network.model.GroupRecord;
 
 import java.util.List;
@@ -12,7 +11,6 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -22,15 +20,18 @@ import rx.Observable;
  */
 
 public interface UserApi {
-    @Headers({"Content-Type: application/json", "Accept: application/json"})
-    @POST("model/submit/{modelType}")
-    Observable<HttpResult<Void>> sumbitModel(
-            @Path("modelType") String modelType,
-            @Body RequestBody modelBody);
+    // 用户登录，返回关于该用户的组信息
+    @GET("user/pull_all_models")
+    Observable<HttpResult<List<MonitoringSite>>> downloadAllModel(@Query("username") String username, @Query("password") String password);
 
-    @GET("model/download")
-    Observable<HttpResult<List<MonitoringSite>>> downloadData(@Query("username") String username,
-                                                              @Query("password") String password);
+
+    // 用户提交数据
+    @Headers({"Content-Type: application/json", "Accept: application/json"})
+    @POST("user/submit_model")
+    Observable<HttpResult<Void>> submitModel(@Query("model_type") String modelType,
+                                             @Query("username") String username,
+                                             @Query("password") String password,
+                                             @Body RequestBody modelBody);
 
     // 用户登录，返回关于该用户的组信息
     @GET("user/login")
@@ -40,6 +41,12 @@ public interface UserApi {
     @GET("user/create_group")
     Observable<HttpResult<GroupRecord>> createNewGroup(@Query("username") String username, @Query("password") String password, @Query("group_name") String name);
 
+    // 用户加入组
     @GET("user/join_group")
     Observable<HttpResult<GroupRecord>> joinGroup(@Query("username") String username, @Query("password") String password, @Query("group_id") String groupId);
+
+    // 请求下载组内所有信息
+    @GET("user/request_tree")
+    Observable<HttpResult<List<MonitoringSite>>> requestTree(@Query("username") String username,
+                                                             @Query("password") String password);
 }
